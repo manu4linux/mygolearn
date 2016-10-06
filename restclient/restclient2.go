8ce95394a63a405760ee0428eb8bf6f84c55199a
mpadur001c <manoharreddy_paduri@comcast.com>
2016-10-06 13:24:43 -0600

    working base client

--- restclient/restclient2.go
+++ restclient/restclient2.go
@@ -1,4 +1,4 @@
-package main
+/*package main
 
 import (
 	"fmt"
@@ -16,3 +16,49 @@ func main() {
 	http.Get(svr.URL)
 	fmt.Println("finished request")
 }
+*/
+
+/*
+//server code 
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+*/
+
+
+/*
+//client code
+
+package main
+
+import (
+	"fmt"
+	"github.com/go-resty/resty"
+)
+
+func main() {
+	// GET request
+	resp, err := resty.R().Get("http://teamcity.cvs-a.ula.comcast.net:8111/guestAuth/app/rest/builds/buildType:(id:CptServers_CptServersLegac_ReleaseBuildTvworksServerParentPom)/number")
+
+	// explore response object
+	fmt.Printf("\nError: %v", err)
+	fmt.Printf("\nResponse Status Code: %v", resp.StatusCode())
+	fmt.Printf("\nResponse Status: %v", resp.Status())
+	fmt.Printf("\nResponse Time: %v", resp.Time())
+	fmt.Printf("\nResponse Recevied At: %v", resp.ReceivedAt())
+	fmt.Printf("\nResponse Body: %v", resp) // or resp.String() or string(resp.Body())
+}
+*/
